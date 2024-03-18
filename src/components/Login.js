7075b822e0f0250409a8efb528839818e97fa9c9
Mishrakumarakhil <akhilmishra184@gmail.com>
2024-03-18 20:34:07 +0530

    Form validation for input box with error message

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,22 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { Header } from "./Header";
+import { checkValidateDate } from "../utils/validate";
 
 export const Login = () => {
   const [isSignIn, setIsSignIn] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("");
+  const email = useRef(null);
+  const password = useRef(null);
 
   const handleButtonCLick = () => {
     //Validate the form data
+    const message = checkValidateDate(
+      email.current.value,
+      password.current.value
+    );
+    setErrorMessage(message);
+
+    // Sign In|| Sign Up
   };
   const toggleSignInForm = () => {
     setIsSignIn(!isSignIn);
@@ -19,7 +30,10 @@ export const Login = () => {
           alt="bg-image"
         />
       </div>
-      <form className="absolute p-12 bg-black w-3/12 my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80 ">
+      <form
+        className="absolute p-12 bg-black w-3/12 my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80 "
+        onSubmit={(e) => e.preventDefault()}
+      >
         <h1 className="font-bold text-3xl py-4">
           {" "}
           {isSignIn ? "Sign In" : "Sign Up"}
@@ -32,15 +46,18 @@ export const Login = () => {
           />
         )}
         <input
+          ref={email}
           type="text"
           placeholder="Email Address"
           className="p-4 my-4 w-full bg-gray-700"
         />
         <input
+          ref={password}
           type="password"
           placeholder="Password"
           className="p-4 my-4 w-full bg-gray-700"
         />
+        <p className="text-red-700 py-2 font-bold text-lg">{errorMessage}</p>
         <button
           className="p-4 my-6 bg-red-700 w-full rounded-lg"
           onClick={handleButtonCLick}
