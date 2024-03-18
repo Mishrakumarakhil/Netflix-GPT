5a950f97011974261df1c008df92bfebfe10ba85
Mishrakumarakhil <akhilmishra184@gmail.com>
2024-03-18 18:45:15 +0530

    Added Project scope in readme file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello World, Build Netflix</div>;
+  return (
+    <div className="text-3xl font-bold text-red-800">
+      Hello World, Build Netflix
+    </div>
+  );
 }
 
 export default App;
