4f4461a8becf91708c4772250bb5022c451961a7
Mishrakumarakhil <akhilmishra184@gmail.com>
2024-03-18 19:47:01 +0530

    Added Sign In Form

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,12 +5,28 @@ export const Login = () => {
   return (
     <div>
       <Header />
-      <div>
+      <div className="absolute">
         <img
           src="https://assets.nflxext.com/ffe/siteui/vlv3/9d3533b2-0e2b-40b2-95e0-ecd7979cc88b/a3873901-5b7c-46eb-b9fa-12fea5197bd3/IN-en-20240311-popsignuptwoweeks-perspective_alpha_website_large.jpg"
           alt="bg-image"
         />
       </div>
+      <form className="absolute p-12 bg-black w-3/12 my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80 ">
+        <h1 className="font-bold text-3xl py-4"> Sign In</h1>
+        <input
+          type="text"
+          placeholder="Email Address"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <button className="p-4 my-6 bg-red-700 w-full rounded-lg">
+          Sign In
+        </button>
+      </form>
     </div>
   );
 };
