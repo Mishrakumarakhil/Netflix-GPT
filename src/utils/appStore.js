c52554f8ca15cd50036110a183e597a913d9c5fb
Mishrakumarakhil <akhilmishra184@gmail.com>
2024-03-20 14:16:29 +0530

    wraaped root component with store

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import userSlice from "./userSlice";
 
 const appStore = configureStore({
-  reducer: {},
+  reducer: {
+    user: userSlice,
+  },
 });
 
 export default appStore;
