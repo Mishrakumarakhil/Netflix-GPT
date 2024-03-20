bdbd5f6113803646c3554bcb5664d15394a489c0
Mishrakumarakhil <akhilmishra184@gmail.com>
2024-03-20 14:51:33 +0530

    added signout functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,38 @@
 import React from "react";
+import { auth } from "../utils/firebase";
+import { signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 export const Header = () => {
+  const navigate = useNavigate();
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        navigate("/");
+      })
+      .catch((error) => {
+        // An error happened.\
+        navigate("/error");
+      });
+  };
   return (
-    <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10">
+    <div className="absolute flex justify-between w-screen px-8 py-2 bg-gradient-to-b from-black z-10">
       <img
         className="w-44 "
         src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
         alt="logo"
       />
+      <div className="flex p-2">
+        <img
+          className="w-12 h-12 "
+          alg="userIcon"
+          src="https://occ-0-4209-3663.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229"
+        />
+        <button onClick={handleSignOut} className="font-bold text-white">
+          Sign Out
+        </button>
+      </div>
     </div>
   );
 };
