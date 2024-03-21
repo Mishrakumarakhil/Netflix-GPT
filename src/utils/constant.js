ca023cdd2de4d921cb360cbaeeb63db36df783e1
Mishrakumarakhil <akhilmishra184@gmail.com>
2024-03-21 14:24:15 +0530

    added tmdb key and get now playing api config

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -4,4 +4,12 @@ export const BG_URL =
   "https://assets.nflxext.com/ffe/siteui/vlv3/9d3533b2-0e2b-40b2-95e0-ecd7979cc88b/a3873901-5b7c-46eb-b9fa-12fea5197bd3/IN-en-20240311-popsignuptwoweeks-perspective_alpha_website_large.jpg";
 
 export const USER_AVATAR =
-  "https://avatars.githubusercontent.com/u/30576273?v=4";
+  "https://occ-0-4209-3663.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229";
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3OTc1OGNjYTJhMzZiOWFhNzIzODczYmJmNzJiZGZkNSIsInN1YiI6IjYwMDgwODJlY2I4MDI4MDAzZWY5YjBmYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Nm-eqUz6ly8u4rn9PjYNZc_hfwR2tUkKaGrR8Wm-mz8",
+  },
+};
