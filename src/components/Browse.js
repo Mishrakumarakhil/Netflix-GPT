637624ba5217c919af3500f1f1456e0ac246978b
Mishrakumarakhil <akhilmishra184@gmail.com>
2024-03-21 14:49:09 +0530

    Created continer for video and movir card

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,12 +1,24 @@
 import React from "react";
 import { Header } from "./Header";
 import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+import { MainContainer } from "./MainContainer";
+import { SecondaryContainer } from "./SecondaryContainer";
 
 export const Browse = () => {
   useNowPlayingMovies();
   return (
     <div>
       <Header />
+      <MainContainer />
+      <SecondaryContainer />
+      {
+        //   MainContaoner
+        //    - Video Background
+        //  -  video Title
+        //   Secondary Container
+        //   - movieList
+        //   - cars*N
+      }
     </div>
   );
 };
