059f379beab3a459b3df927b40af93913921d2a7
Mishrakumarakhil <akhilmishra184@gmail.com>
2024-03-26 12:15:57 +0530

    build video backgroun container

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -8,8 +8,9 @@ export const VideoBackground = ({ movieId }) => {
   useTarilerVideo(movieId);
 
   return (
-    <div>
+    <div className="w-screen">
       <iframe
+        className="w-screen aspect-video"
         src={"https://www.youtube.com/embed/" + tarilerId}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
