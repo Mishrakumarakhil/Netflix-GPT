a6d13442ab7a9ee1d2da59f0e27cf83dd1582363
Mishrakumarakhil <akhilmishra184@gmail.com>
2024-03-26 12:17:23 +0530

    added hover for video button

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -6,10 +6,10 @@ export const VideoTitle = ({ title, overview }) => {
       <h1 className="text-6xl font-bold">{title}</h1>
       <p className="py-6 text-lg w-1/4">{overview}</p>
       <div className="">
-        <button className="bg-white text-black p-4 px-10  text-xl  rounded-lg">
+        <button className="bg-white text-black p-4 px-10  text-xl  rounded-lg hover:bg-opacity-80">
           ▶️ Play
         </button>
-        <button className="mx-2 bg-gray-500 text-white p-4 px-10  text-xl bg-opacity-50 rounded-lg">
+        <button className="mx-2 bg-gray-500 text-white p-4 px-10  text-xl bg-opacity-50 rounded-lg hover:bg-opacity-80">
           More Info
         </button>
       </div>
