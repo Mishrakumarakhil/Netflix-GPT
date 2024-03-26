2c1d7d9be7a1cb21504f0271af4c4fc5832c73d0
Mishrakumarakhil <akhilmishra184@gmail.com>
2024-03-26 12:19:58 +0530

    Added autoplay on video with mute

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -11,7 +11,9 @@ export const VideoBackground = ({ movieId }) => {
     <div className="w-screen">
       <iframe
         className="w-screen aspect-video"
-        src={"https://www.youtube.com/embed/" + tarilerId}
+        src={
+          "https://www.youtube.com/embed/" + tarilerId + "?&autoplay=1&mute=1"
+        }
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerPolicy="strict-origin-when-cross-origin"
